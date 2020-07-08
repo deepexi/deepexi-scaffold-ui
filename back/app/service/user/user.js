55e99bcb111a6f29ae7852f572c7ee8df60f5dba
whohim <huqian@deepexi.com>
2020-07-08 14:56:19 +0800

    refactor: 修改token为userId

--- back/app/service/user/user.js
+++ back/app/service/user/user.js
@@ -18,19 +18,19 @@ class User {
 
   async login(username, password) {
     if (username === adminName && password === adminPassWord) {
-      const token = this.ctx.session.token = uuidv1();
-      this.ctx.cookies.set('token', token);
-      this.ctx.logger.debug('[User] %s --- 登录', token);
+      const userId = this.ctx.session.userId = uuidv1();
+      this.ctx.cookies.set('userId', userId);
+      this.ctx.logger.debug('[User] %s --- 登录', userId);
       return username;
     }
     throw new UserError('用户名或密码错误!');
   }
 
   async logout() {
-    const token = this.ctx.session.token;
-    const userToken = this.ctx.cookies.get('token');
-    if (token === userToken) {
-      this.ctx.logger.debug('[User] %s --- 登出', token);
+    const userId = this.ctx.session.userId;
+    const userUserId = this.ctx.cookies.get('userId');
+    if (userId  === userUserId) {
+      this.ctx.logger.debug('[User] %s --- 登出', userUserId);
       return true;
     }
     throw new UserError('用户未登录!');
